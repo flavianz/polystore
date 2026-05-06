76198cfdfbe0f3fa2b6bb0afc145a4c69bea99cb
Flavian Züllig <flavianzuellig@hotmail.com>
2026-05-06 20:40:32 +0200

    create query interface

---
+++ src/main/kotlin/query/CreateQuery.kt
@@ -0,0 +1,5 @@
+package ch.flavianz.query
+
+import ch.flavianz.model.CollectionModel
+
+data class CreateQuery(val collectionModel: CollectionModel) : Query
---
+++ src/main/kotlin/query/Query.kt
@@ -0,0 +1,4 @@
+package ch.flavianz.query
+
+interface Query {
+}
