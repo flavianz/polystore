3f62b8c39e853be1597a991f3098875ade8df9f1
Flavian Züllig <flavianzuellig@hotmail.com>
2026-05-10 13:10:45 +0200

    add build configuration caching and logging implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     implementation("org.postgresql:postgresql:42.7.11")
     implementation("org.mongodb:mongodb-driver-sync:5.1.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
 
     testImplementation(kotlin("test"))
 }
