a4e290f9ff24b7ae0b499155438f0de50046611d
Flavian Züllig <flavianzuellig@hotmail.com>
2026-08-13 10:59:49 +0200

    add nicer toString for conditions

--- server/src/main/kotlin/query/Condition.kt
+++ server/src/main/kotlin/query/Condition.kt
@@ -5,20 +5,51 @@ sealed class Condition {
         abstract val field: String
         abstract val value: Any?
 
-        data class Equals(override val field: String, override val value: Any?) : Comparison()
-        data class GreaterThan(override val field: String, override val value: Number) : Comparison()
-        data class LessThan(override val field: String, override val value: Number) : Comparison()
+        data class Equals(override val field: String, override val value: Any?) : Comparison() {
+            override fun toString(): String {
+                return "'${field}' = $value"
+            }
+        }
+
+        data class GreaterThan(override val field: String, override val value: Number) : Comparison() {
+            override fun toString(): String {
+                return "'$field' > $value"
+            }
+        }
+
+        data class LessThan(override val field: String, override val value: Number) : Comparison() {
+            override fun toString(): String {
+                return "'$field' < $value"
+            }
+        }
     }
 
     sealed class Logic : Condition() {
         abstract val left: Condition
         abstract val right: Condition
 
-        data class And(override val left: Condition, override val right: Condition) : Logic()
-        data class Or(override val left: Condition, override val right: Condition) : Logic()
+        data class And(override val left: Condition, override val right: Condition) : Logic() {
+            override fun toString(): String {
+                return "$left and $right"
+            }
+        }
+
+        data class Or(override val left: Condition, override val right: Condition) : Logic() {
+            override fun toString(): String {
+                return "$left or $right"
+            }
+        }
     }
 
-    data class Not(val condition: Condition) : Condition()
+    data class Not(val condition: Condition) : Condition() {
+        override fun toString(): String {
+            return "not $condition"
+        }
+    }
 
-    data class In(val field: String, val list: Set<Any?>) : Condition()
+    data class In(val field: String, val list: Set<Any?>) : Condition() {
+        override fun toString(): String {
+            return "'$field' in [${list.joinToString { ", " }}]"
+        }
+    }
 }
